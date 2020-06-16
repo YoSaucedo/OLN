aef457b8c4de12bdd59aa8d602b686773062c770
Enrique Saucedo Fuentes <enrique@yutsil.com>
2020-06-15 20:54:46 -0500

    Initial OLN

---
+++ contracts/OLN/token/OLN/OLN.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.2;
+
+import "./ERC721.sol";
+import "../../access/roles/MinterRole.sol";
+
+/**
+ * @title ERC721Mintable
+ * @dev ERC721 minting logic
+ */
+contract OLN is ERC721, MinterRole {
+  /**
+   * @dev Function to mint tokens
+   * @param to The address that will receive the minted tokens.
+   * @param tokenId The token id to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(
+    address to,
+    uint256 tokenId
+  )
+    public
+    onlyMinter
+    returns (bool)
+  {
+    _mint(to, tokenId);
+    return true;
+  }
+}
